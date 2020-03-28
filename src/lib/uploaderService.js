32901d8d395f10b3718415dfeb367e63615d6603
Sami Agoudan <agoudansami@gmail.com>
2020-03-29 00:41:11 +0100

    Refactor uploaderService

--- src/lib/uploaderService.js
+++ src/lib/uploaderService.js
@@ -1,55 +1,54 @@
+const fs = require("fs");
 const skynet = require('@nebulous/skynet');
 const databaseService = require('./databaseService');
 const encryptorService = require('./encryptorService')
-const fs = require("fs");
 
 const db = new databaseService()
 const encryptor = new encryptorService()
 
 class UploaderService {
 
-  async uploadToSkynet(file) {
+  async uploadToSkynet(file, encryptionType) {
     const skylink = await skynet.UploadFile(
       file,
       skynet.DefaultUploadOptions
-    )
+    ).then((res) => {
+      console.log(res)
+      db.add('history', { skylink: res, encryption: encryptionType, type: 'upload' })
+    })
     return skylink
   }
 
   async uploadFile(filePath, data) {
-
-    if (data.encryptionType === 'none') {
-      try {
-        const skylink = await this.uploadToSkynet(filePath)
-        db.add('history', { skylink: skylink, encryption: data.encryptionType, type: 'upload' })
-        return { skylink: skylink, encryption: data.encryptionType }
-      } catch (e) {
-        return 'noneUploadFailed'
-      }
-
-    } else if (data.encryptionType === 'symmetric') {
-      try {
-        const encryptedFile = await encryptor.symmetricEncryption(filePath, data.password)
-        const skylink = await this.uploadToSkynet(encryptedFile)
-        fs.unlinkSync(encryptedFile)
-        db.add('history', { skylink: skylink, encryption: data.encryptionType, type: 'upload' })
-        return { skylink: skylink, encryption: data.encryptionType };
-      } catch (e) {
-        return 'symmetricUploadFailed'
-      }
-    } else if (data.encryptionType === 'asymmetric') {
-      try {
-        const encryptedFile = await encryptor.asymmetricEncryption(filePath, data.publicAddress)
-        const skylink = await this.uploadToSkynet(encryptedFile)
-        fs.unlinkSync(encryptedFile)
-        db.add('history', { skylink: skylink, encryption: data.encryptionType, type: 'upload' })
-        return { skylink: skylink, encryption: data.encryptionType }
-      } catch (e) {
-        //throw e
-        return 'asymmetricUploadFailed'
-      }
+    switch(data.encryptionType) {
+      case 'none':
+        try {
+          const skylink = await this.uploadToSkynet(filePath, data.encryptionType)
+          return { skylink: skylink, encryption: data.encryptionType }
+        } catch (e) {
+          return 'noneUploadFailed'
+        }
+      case 'symmetric':
+        try {
+          const encryptedFile = await encryptor.symmetricEncryption(filePath, data.password)
+          const skylink = await this.uploadToSkynet(encryptedFile, data.encryptionType)
+          fs.unlinkSync(encryptedFile)
+          return { skylink: skylink, encryption: data.encryptionType };
+        } catch (e) {
+          return 'symmetricUploadFailed'
+        }
+      case 'asymmetric':
+        try {
+          const encryptedFile = await encryptor.asymmetricEncryption(filePath, data.publicAddress)
+          const skylink = await this.uploadToSkynet(encryptedFile, data.encryptionType)
+          fs.unlinkSync(encryptedFile)
+          return { skylink: skylink, encryption: data.encryptionType }
+        } catch (e) {
+          //throw e
+          return 'asymmetricUploadFailed'
+        }
     }
-  }
+  } 
 }
 
 module.exports = UploaderService;
